e9e82008fb1ca6da86e0edf491c9b6d918ab3b6a
smeyer <smeyer.ilias@gmx.de>
2022-07-08 12:46:37 +0200

    veda: increased version

--- plugin.php
+++ plugin.php
@@ -3,13 +3,13 @@
 $id = "vedaimp";
 
 // code version; must be changed for all code changes
-$version = "6.10.4";
+$version = "7.11.0";
 
 // ilias min and max version; must always reflect the versions that should run with the plugin
-$ilias_min_version = "6.1";
-$ilias_max_version = "6.99";
+$ilias_min_version = "7.10";
+$ilias_max_version = "7.99";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Stefan Meyer';
-$responsible_mail = 'smeyer.ilias@gmx.de';
+$responsible_mail = 'meyer@leifos.de';
 ?>
